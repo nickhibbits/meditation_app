39edfac187b404644a4f3e7131bebc8a3dff78b5
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-20 14:15:26 -0700

    add link capability to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,16 @@
 import styles from "@/styles/components/Button.module.scss";
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
 
-function Button({ text }: { text: string }) {
+function Button({ text, link }: { text: string; link: Url }) {
+  if (link) {
+    return (
+      <Link href={link} className={styles.button_component}>
+        {text}
+      </Link>
+    );
+  }
+
   return <div className={styles.button_component}>{text}</div>;
 }
 
