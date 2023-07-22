e6c623ae25193d39f7e727b950715c855b8cac07
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-21 19:21:28 -0700

    continue session options styling

--- components/session_options/LocationSelector.tsx
+++ components/session_options/LocationSelector.tsx
@@ -1,7 +1,15 @@
 import styles from "@/styles/pages/SessionOptions.module.scss";
 
 function LocationSelector() {
-  return <div className={styles.selector_component}>Location Selector</div>;
+  return (
+    <div className={styles.selector_component}>
+      <ul className={styles.options_wrapper}>
+        <li className={styles.option}>Ocean</li>
+        <li className={styles.option}>Mountains</li>
+        <li className={styles.option}>Jungle</li>
+      </ul>
+    </div>
+  );
 }
 
 export default LocationSelector;
