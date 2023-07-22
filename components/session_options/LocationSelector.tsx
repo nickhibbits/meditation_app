4ec1e97abc4cdd92cc1283dac41e9ef3dcad64a1
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-21 19:35:09 -0700

    checkpoint

--- components/session_options/LocationSelector.tsx
+++ components/session_options/LocationSelector.tsx
@@ -2,7 +2,7 @@ import styles from "@/styles/pages/SessionOptions.module.scss";
 
 function LocationSelector() {
   return (
-    <div className={styles.selector_component}>
+    <div className={`flex flex_center ${styles.selector_component}`}>
       <ul className={styles.options_wrapper}>
         <li className={styles.option}>Ocean</li>
         <li className={styles.option}>Mountains</li>
