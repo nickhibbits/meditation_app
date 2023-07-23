4670aaa425fb49dc07aeb2725f8467b350f5ac68
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-22 22:17:25 -0700

    tighten circle menu concept

--- components/session_options/LocationSelector.tsx
+++ components/session_options/LocationSelector.tsx
@@ -3,11 +3,11 @@ import styles from "@/styles/pages/SessionOptions.module.scss";
 function LocationSelector() {
   return (
     <div className={`flex flex_center ${styles.selector_component}`}>
-      <ul className={styles.options_wrapper}>
-        <li className={styles.option}>Ocean</li>
-        <li className={styles.option}>Mountains</li>
-        <li className={styles.option}>Jungle</li>
-      </ul>
+      {/* <div className={styles.middle_circle}> o </div> */}
+      <div className={styles.middle_circle} />
+      <li className={styles.option}>Ocean</li>
+      <li className={styles.option}>Mountains</li>
+      <li className={styles.option}>Jungle</li>
     </div>
   );
 }
