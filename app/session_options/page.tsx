c50669a6847cb7df8fd153cb8edf2651d74d4648
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-24 11:22:44 -0700

    add NavButton component

--- app/session_options/page.tsx
+++ app/session_options/page.tsx
@@ -1,11 +1,16 @@
+import NavButton from "@/components/NavButton";
 import SessionOption from "@/components/session_options/SessionOption";
 import styles from "@/styles/pages/SessionOptions.module.scss";
+import { relative } from "path";
 
 const sessionOptions = ["Location", "Duration"];
 
 function SessionOptions() {
   return (
-    <main className="container flex flex_center">
+    <main
+      className="container flex flex_center flex_column"
+      style={{ position: "relative" }}
+    >
       <div className={`flex ${styles.options_wrapper}`}>
         {sessionOptions.map((sessionOption, i) => {
           return (
@@ -15,6 +20,10 @@ function SessionOptions() {
             </div>
           );
         })}
+      </div>
+      <div className={`flex flex_row ${styles.nav_button_wrapper}`}>
+        <NavButton url="/" text="Back" justifyContent="flex-start" />
+        <NavButton url="/session" text="Start" justifyContent="flex-end" />
       </div>
     </main>
   );
