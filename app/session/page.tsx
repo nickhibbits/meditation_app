56237cc3bff565b1fe9db14279096d19c145ec04
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-07-24 14:07:57 -0700

    transfer duration to session page, start timer

--- app/session/page.tsx
+++ app/session/page.tsx
@@ -1,6 +1,26 @@
+"use client";
+
+import Timer from "@/components/Timer";
+import { useSearchParams } from "next/navigation";
+
 function Session() {
+  const params = useSearchParams();
+  const duration = params.get("duration");
+  const location = params.get("locastion");
+
+  console.log({ location, duration });
+
+  const _duration = typeof duration === "string" ? Number(duration) : 0;
+
+  const time = new Date();
+  const expiryTimestamp = new Date(
+    time.setSeconds(time.getSeconds() + _duration * 60)
+  );
+
   return (
-    <main className="container flex flex_column flex_center">Session</main>
+    <main className="container flex flex_column flex_center">
+      <Timer expiryTimestamp={expiryTimestamp} />
+    </main>
   );
 }
 
