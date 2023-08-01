af9a0ced3e7c8a6a59beaa1508e9a806b48f505d
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-08-01 13:19:50 -0700

    add blob svgs to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
 import Button from "@/components/Button";
 
+import styles from "@/styles/pages/Home.module.scss";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="container flex flex_column flex_center">
+    <main
+      className={`${styles.home_page} container flex flex_column flex_center`}
+    >
+      <Image
+        src="/svg/blob_1.svg"
+        alt="blob"
+        className={styles.blob_1}
+        height={800}
+        width={800}
+      />
+      <Image
+        src="/svg/blob_2.svg"
+        alt="blob"
+        className={styles.blob_2}
+        height={700}
+        width={700}
+      />
+      <Image
+        src="/svg/blob_3.svg"
+        alt="blob"
+        className={styles.blob_3}
+        height={400}
+        width={400}
+      />
       <Button
         text={"Start Session"}
         url={"/session_options"}
