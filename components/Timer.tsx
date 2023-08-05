2aa5e5b0aa76ddfd66cc218f4aa2efa7ccf2ed14
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-08-05 10:10:11 -0700

    disable time autostart, style UI accordingly

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -5,10 +5,12 @@ import Button from "@/components/Button";
 import styles from "@/styles/components/Timer.module.scss";
 
 function Timer({ expiryTimestamp }: { expiryTimestamp: Date }) {
-  const { seconds, minutes, isRunning, start, pause, resume } = useTimer({
-    expiryTimestamp,
-    onExpire: () => console.warn("onExpire called"),
-  });
+  const { seconds, minutes, isRunning, start, pause, resume, restart } =
+    useTimer({
+      expiryTimestamp,
+      autoStart: false,
+      onExpire: () => console.warn("onExpire called"),
+    });
 
   return (
     <div style={{ textAlign: "center", zIndex: "2" }}>
@@ -18,10 +20,9 @@ function Timer({ expiryTimestamp }: { expiryTimestamp: Date }) {
       </div>
       <p>{isRunning ? "In session" : "Paused"}</p>
       <div className={`flex ${styles.buttons_wrapper}`}>
-        <Button text="Start" url={""} onClick={start} justifyContent="center" />
         <Button text="Pause" url={""} onClick={pause} justifyContent="center" />
         <Button
-          text="Resume"
+          text="Start"
           url={""}
           onClick={resume}
           justifyContent="center"
