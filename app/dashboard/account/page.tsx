cc8f7d10e6f263f7db4d53496dec890deda8b045
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-08-28 21:37:07 -0700

    init account page

---
+++ app/dashboard/account/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Account() {
+  return <div>ACCOUNT</div>;
+}
+
+export default Account;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 import styles from "@/styles/components/Nav.module.scss";
 
 function NavBar({ user }: { user: any }) {
+  const router = useRouter();
+
   const handleSignOut = () => {
     signOut();
     redirect("/");
@@ -14,7 +16,12 @@ function NavBar({ user }: { user: any }) {
   return (
     <nav className={styles.nav}>
       <div className={styles.nav_options_wrapper}>
-        <li className={styles.user}>{user}</li>
+        <li
+          className={styles.user}
+          onClick={() => router.push("/dashboard/account")}
+        >
+          {user}
+        </li>
         <li className={styles.logout} onClick={() => handleSignOut()}>
           logout
         </li>
